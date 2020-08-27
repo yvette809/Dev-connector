8ead69bb8885d49b4b109c2e35da5c72103825b6
Yvette <nchombuayvta@gmil.com>
2020-08-28 01:07:03 +0200

    get user profile

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,75 @@
 const express = require ("express");
 const authRouter = express.Router();
-
+const bcrypt = require ("bcryptjs")
+const auth = require ('../../routes/api/middleware/auth')
+const jwt = require("jsonwebtoken")
+const UserModel = require("./models/userSchema");
+const UsersModel = require("./models/userSchema");
 // @route GET api/auth
 // @desc Test route
 // @access public
-authRouter.get("/", (req,res)=> res.send('user route'))
+
+authRouter.get("/", auth, async(req,res,next)=>{
+    try{
+        const user = await UsersModel.findById(req.user.id).select ('-password')
+        if(user){
+        res.json(user);
+
+        }else{
+            const error = new Error("user not found")
+            error.httpStatusCode =404
+            next(error)
+
+        }
+
+    }catch(error){
+        next(error)
+    }
+
+})
+
+// login user
+// @route POST api/auth
+//@desc    Authenticate user and get a token
+// @ access public
+authRouter.post("/", async(req,res, next)=> {
+    
+    const {email,password} = req.body;
+    try{
+          // see if user exists
+       let user = await UserModel.findOne({email});
+       if(!user){
+           res.status(400).json({errors:[{msg:"invalid credentials"}]})
+       }else{
+
+         const isMatch = await bcrypt.compare(password,user.password)
+         if(!isMatch){
+            res.status(400).json({errors:[{msg:"invalid credentials"}]})
+
+         }
+
+         // Return jsonwebtoken
+         const payload ={
+             user:{
+                 id:user.id
+             }
+         }
+         jwt.sign(payload,process.env.jwt_Secret, {expiresIn:360000} ,(err,token)=>{
+             if(err){
+                 throw err
+             }else{
+                res.json({token})
+            }
+         })
+
+       };
+
+
+    }catch(error){
+        next(error)
+    }
+})
+
 
 
 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,33 @@
 const express = require ("express");
 const profileRouter = express.Router();
+const ProfileModel = require("./models/profileSchema")
+const UserModel = require('./models/userSchema')
+const auth = require('../../routes/api/middleware/auth')
 
-// @route GET api/users
-// @desc Test route
-// @access public
-profileRouter.get("/", (req,res)=> res.send('user route'))
+// @route GET api/profile/me
+// @desc get current users profile
+// @access private
+profileRouter.get("/me", auth , async(req,res,next)=>{
+    try{
+        const profile = await  ProfileModel.findOne({user:req.user.id}).populate('user',['name', 'avatar'])
+        if(!profile){
+            return res.status(400).json({msg:'There is no profile for this user'})
+        }else{
+            res.json(profile)
+        }
+    }catch(error){
+        next(error)
+    }
+
+})
+
+//@route   POST api/profile
+// @desc create or update user profile
+// @access  private
+
+profileRouter.post("/", async(rew,res,next)=>{
+
+})
 
 
 
