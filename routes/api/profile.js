fb823b1170edbd0ebca5da730464e1cbd6005bac
Yvette <nchombuayvta@gmil.com>
2020-08-31 17:56:14 +0200

    new profile created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -25,8 +25,78 @@ profileRouter.get("/me", auth , async(req,res,next)=>{
 // @desc create or update user profile
 // @access  private
 
-profileRouter.post("/", async(rew,res,next)=>{
+profileRouter.post("/", auth,async(req,res,next)=>{
 
+    const{
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githuusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body;
+
+    // build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(company)  profileFields.company= company;
+    if(website)  profileFields.website= website;
+    if(location)  profileFields.location= location;
+    if(bio)  profileFields.bio= bio;
+    if(status)  profileFields.status= status;
+    if(githuusername)  profileFields.githuusername= githuusername;
+    if(skills){
+        profileFields.skills = skills.split(',').map(skill =>skill.trim())
+    }
+    console.log(profileFields.skills)
+
+    // build socil object
+    profileFields.social = {}
+    if(youtube)profileFields.social.youtube= youtube;
+    if(twitter)profileFields.social.twitter= twitter;
+    if(facebook)profileFields.social.facebook = facebook;
+    if(linkedin) profileFields.social.linkedin = linkedin;
+    if(instagram)profileFields.social.instagram = instagram;
+
+    try{
+
+        let profile = await ProfileModel.findOne({user:req.user.id})
+        if(profile){
+            //update
+            profile = await profile.findOneAndUpdate({user:req.user.id}, {$set:profileFields},{new:true})
+            return res.json(profile)
+
+        }else{
+            // create profile
+            const profile= new ProfileModel(profileFields)
+            await profile .save()
+            res.json(profile)
+        }
+
+    }catch(error){
+        next(error)
+    }
+
+})
+
+
+
+//@route   GET api/profile
+// @desc Get all profiles
+// @access  public
+
+profileRouter.get("/", async(req,res,next)=>{
+    try{
+
+    }catch(error){
+        next(error)
+    }
 })
 
 
