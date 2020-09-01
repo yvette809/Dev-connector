b2944df57ead26119d548865eb41157a8518edc4
Yvette <nchombuayvta@gmil.com>
2020-09-01 14:03:37 +0200

    commnts added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -128,8 +128,8 @@ postRouter.put('/unlike/:id', auth, async(req,res,next)=>{
         } else{
             //get removeindex
 
-            const removeIndex = post.likes.map(like=> like.user.toString()).indexOf(req.user.id)
-            post.likes.splice(removeIndex,1)
+        const removeIndex = post.likes.map(like=> like.user.toString()).indexOf(req.user.id)
+        post.likes.splice(removeIndex,1)
 
         await post.save()
         res.json(post.likes)
@@ -141,4 +141,62 @@ postRouter.put('/unlike/:id', auth, async(req,res,next)=>{
     }
 })
 
+
+// @route POST api/posts/comment/:id
+// @desc comment on post
+// @access private
+postRouter.post("/comment/:id", auth, async (req,res,next)=> {
+    try {
+        const user  = await UserModel.findById(req.user.id).select('-password');
+        const post = await PostModel.findById(req.params.id)
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+    
+        }
+
+        post.comments.unshift(newComment)
+        await post.save()
+        res.json(post.comments)
+    } catch (error) {
+        next(error)
+        
+    }
+   
+})
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Delete Comment
+// @access private
+
+postRouter.delete('/comment/:id/:comment_id', auth,async(req,res,next)=>{
+    try {
+        const post = await PostModel.findById(req.params.id);
+        // pull out comment
+        const comment = post.comments.find(comment=>comment.id=== req.params.comment_id)
+        // Make sure comment exists
+        if(!comment){
+            return res.status(404).json({msg:'Comment does not exist'})
+        }
+
+        // check user
+        if(comment.user.toString()!== req.user.id){
+            return res.status(404).json({msg: 'User not authoried'})
+        }
+
+        // get remove index
+
+        const removeIndex = post.comments.map(comment=> comment.user.toString()).indexOf(req.user.id)
+        post.comments.splice(removeIndex,1)
+
+        await post.save()
+        res.json(post.comments)
+        
+    } catch (error) {
+        next(error)
+        
+    }
+})
 module.exports =  postRouter
